fcf9327db24b22090900e6486dfb82b7eecb7586
Intercept <intercept.gatech@gmail.com>
2017-12-02 14:12:30 +0000

    Adding flask_cors library and functionality to enable localhost to hit the backend

--- add_questions.py
+++ add_questions.py
@@ -1,11 +1,10 @@
 import json
-from flask_pymongo import pymongo
-from pymongo import MongoClient
+from PyMongo import MongoClient
 
 with open('questions.json', encoding="utf8") as data_file:    
     data = json.load(data_file)
 
-client = pymongo.MongoClient()
+client = MongoClient()
 db = client.interceptDB
 collection = db.questions
 collection.insert_many(data)
