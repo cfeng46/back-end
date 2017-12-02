367e8fc8ae28a5e99da429943fd0140d29bdad5b
William Gao <william.gaom@gmail.com>
2017-12-02 09:45:27 -0500

    Made changes to updated_current_questions. Fixed major errors such as cyclical questions and duplication of questionID 702

--- add_questions.py
+++ add_questions.py
@@ -2,10 +2,10 @@ import json
 from flask_pymongo import pymongo
 from pymongo import MongoClient
 
-with open('questions.json', encoding="utf8") as data_file:    
+with open('updated_current_questions.json', encoding="utf8") as data_file:
     data = json.load(data_file)
 
-client = pymongo.MongoClient()
+client = pymongo.MongoClient("mongodb://admin:intercept@45.55.198.145/interceptDB")
 db = client.interceptDB
 collection = db.questions
 collection.insert_many(data)
