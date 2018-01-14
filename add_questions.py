5d1f4873724d13de0ca4382148c01213f36be9df
DomKynkor <domkynkor@gmail.com>
2018-01-13 21:05:00 -0500

    fixed add_questions.py script
    changed "PyMongo" in import statement to "pymongo"
    removed "pymongo" in "pymongo.MongoClient(......)"

--- add_questions.py
+++ add_questions.py
@@ -1,10 +1,10 @@
 import json
-from PyMongo import MongoClient
+from pymongo import MongoClient
 
 with open('updated_current_questions.json', encoding="utf8") as data_file:
     data = json.load(data_file)
 
-client = pymongo.MongoClient("mongodb://admin:intercept@45.55.198.145/interceptDB")
+client = MongoClient("mongodb://admin:intercept@45.55.198.145/interceptDB")
 db = client.interceptDB
 collection = db.questions
 collection.insert_many(data)
