4427049581388e956655ab24b03565516d9d6933
cfeng46 <cfeng7@crimson.ua.edu>
2018-03-17 09:45:12 -0400

    Revert "fixed add_questions.py script" unexpected commit to master This reverts commit 5d1f4873724d13de0ca4382148c01213f36be9df.

--- add_questions.py
+++ add_questions.py
@@ -1,10 +1,10 @@
 import json
-from pymongo import MongoClient
+from PyMongo import MongoClient
 
 with open('updated_current_questions.json', encoding="utf8") as data_file:
     data = json.load(data_file)
 
-client = MongoClient("mongodb://admin:intercept@45.55.198.145/interceptDB")
+client = pymongo.MongoClient("mongodb://admin:intercept@45.55.198.145/interceptDB")
 db = client.interceptDB
 collection = db.questions
 collection.insert_many(data)
